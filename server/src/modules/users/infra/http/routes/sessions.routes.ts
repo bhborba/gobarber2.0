9bcb9808c0db4c76d8c40d869d09417fae30d0ff
Bruno Henrique de Borba <brunohenriqueborba@gmail.com>
2021-02-08 07:13:14 -0300

    Refactoring users module

--- server/src/modules/users/infra/http/routes/sessions.routes.ts
+++ server/src/modules/users/infra/http/routes/sessions.routes.ts
@@ -1,13 +1,15 @@
 import { Router } from 'express';
 
 import CreateSessionService from '@modules/users/services/CreateSessionService';
+import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 
 const sessionsRouter = Router();
 
 sessionsRouter.post('/', async (req, res) => {
   const { email, password } = req.body;
 
-  const createSession = new CreateSessionService();
+  const usersRepository = new UsersRepository();
+  const createSession = new CreateSessionService(usersRepository);
 
   const { user, token } = await createSession.execute({
     email,
--- server/src/modules/users/services/CreateSessionService.ts
+++ server/src/modules/users/services/CreateSessionService.ts
@@ -1,4 +1,3 @@
-import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 import AppError from '@shared/errors/AppError';
@@ -6,21 +5,23 @@ import authConfig from '@config/auth';
 
 import User from '../infra/typeorm/entities/User';
 
-interface Request {
+import IUsersRepository from '../repositories/IUsersRepository';
+
+interface IRequest {
   email: string;
   password: string;
 }
 
-interface Response {
+interface IResponse {
   user: User;
   token: string;
 }
 
 class CreateSessionService {
-  public async execute({ email, password }: Request): Promise<Response> {
-    const usersRepository = getRepository(User);
+  constructor(private usersRepository: IUsersRepository) {}
 
-    const user = await usersRepository.findOne({ where: { email } });
+  public async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email);
 
     if (!user) {
       throw new AppError('Invalid email or password', 401);
